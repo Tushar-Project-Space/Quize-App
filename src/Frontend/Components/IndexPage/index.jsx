4054509240436b8871a6e6dbe249fbdd7318f7da
Tushar More <158036610+Tushar-Project-Space@users.noreply.github.com>
2025-05-07 16:16:51 +0530

    setup

---
+++ src/Frontend/Components/IndexPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <p>Online Quize App</p>
+      <p>copyright by Tushar More</p>
+    </div>
+  );
+};
+
+export default Index;
